689cc2d30aaa1e4291ea0c67f0a999b5bc405719
josanmontalban <josanmontalban@gmail.com>
2025-10-07 19:00:55 +0200

    update

--- Inicial/src/Main.java
+++ Inicial/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+    //Esto es un comentario para aclaracion breve de lo que hace el metodo
+
+    /**
+     * Comentario de documentacion Javadoc
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Ejemplo");
+        String nombreCompleto = "José Andrés Montalbán Guerra"; //Los String son palabras que van entre comillados dobles
+        int edad = 27;
+        System.out.println("Hola, este es mi primer programa en JAVA. \nEsta es la segunda linea\n");
+        System.out.print("Mi nombre es"+nombreCompleto);
+        System.out.print("Mi edad es"+edad;
+        /*
+        Este tipo de comentraio
+        te permite escribir
+        en mas de una linea
+         */
     }
+    public void metodoCarton()
+        //TODO pendiente crear el carton con numeros aleatorios sin repetir
 }
